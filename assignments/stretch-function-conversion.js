767df22bc07882d86dc3ea8feffb8312f830fc40
mark-marshall <mark.marshallgp@gmail.com>
2019-01-21 13:34:33 +0000

    [STRETCH] Refactor

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -25,7 +25,6 @@ console.log(add(1, 2));
 
 let subtract = (param1, param2) => param1 - param2;
 console.log(subtract(1, 2));
-s;
 exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
